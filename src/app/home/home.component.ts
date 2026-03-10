bde307f7298af609ac92d41be31fb6d08b89d2f4
Szymon Gross <szyg@netcompany.com>
2026-03-10 17:31:00 +0100

    Permanently link template history

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,20 @@
 import {Component, OnInit} from '@angular/core';
-import {environment} from "../../environments/environment";
+import {environment} from '../../environments/environment';
 import {CommonModule} from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import {WarehouseViewComponent} from './warehouse/warehouse.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, WarehouseViewComponent]
 })
 export class HomeComponent implements OnInit {
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   protected readonly environment = environment;
 }
